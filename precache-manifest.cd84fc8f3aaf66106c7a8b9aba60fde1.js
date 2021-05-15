f48730da1b4f6e1479c333d73d30d74fbfa720ad
KrantiBrid <kranti.brid@azularc.com>
2021-05-15 20:04:25 +0530

    Updates

---
+++ precache-manifest.cd84fc8f3aaf66106c7a8b9aba60fde1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "959bcc87337fe9a61a6a400edb17ce00",
+    "url": "/Marvel/index.html"
+  },
+  {
+    "revision": "a5416b688928423262ce",
+    "url": "/Marvel/static/css/main.80deb305.chunk.css"
+  },
+  {
+    "revision": "792bb1552bf0b0dc508f",
+    "url": "/Marvel/static/js/2.0f4953ae.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/Marvel/static/js/2.0f4953ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5416b688928423262ce",
+    "url": "/Marvel/static/js/main.fee48245.chunk.js"
+  },
+  {
+    "revision": "77c59da2fcea4a0fca18",
+    "url": "/Marvel/static/js/runtime-main.b56a8916.js"
+  },
+  {
+    "revision": "067285b0c7e98187bfeb0fe492712d2e",
+    "url": "/Marvel/static/media/Marvel.067285b0.ttf"
+  },
+  {
+    "revision": "d534c1f64330c99fcc4f85d052d59f34",
+    "url": "/Marvel/static/media/comment-bg.d534c1f6.jpg"
+  },
+  {
+    "revision": "99e95eaf9ab58d4d52ccfc65dc8dd1aa",
+    "url": "/Marvel/static/media/imageLoaded.99e95eaf.gif"
+  }
+]);
